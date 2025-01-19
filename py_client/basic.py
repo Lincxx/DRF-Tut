2d3599b43140f9b6192bb33fc2fc856b4d78b0e0
Lincxx <bossco28@gmail.com>
2025-01-19 07:58:15 -0500

    Add .gitignore file

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,5 +4,6 @@ endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint)
 print(get_response.text)
-# print(get_response.json())
 print(get_response.status_code)
+
+# print(get_response.json())
